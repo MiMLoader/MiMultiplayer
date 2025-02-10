fbca5a7fea6467c00bc80d13f1ffdaa446feabdb
Saturn Maoudis <saturnmaoudis@protonmail.com>
2025-02-10 05:46:09 +0000

    debug

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -114,6 +114,7 @@ const server = new Elysia()
                         name: self.name,
                         instVars: data
                     };
+                    console.log(playerObj, data)
                     this.world.players.current.set(id, playerObj);
                     publish('syncPlayer', { channel, playerObj });
                 }
